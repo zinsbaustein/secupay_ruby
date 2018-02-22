8bbcdeb7240fabc2ab6bf8e4145b1e9686b9dcbe
Jörg Schiller <joergschiller@googlemail.com>
2018-02-22 14:25:02 +0100

    Use named subject in request error spec.

--- spec/request_error_spec.rb
+++ spec/request_error_spec.rb
@@ -1,24 +1,24 @@
 require "spec_helper"
 
 describe SecupayRuby::RequestError do
-  subject { described_class.new(errors) }
+  subject(:request_error) { described_class.new(errors) }
 
   let(:errors) { 'Request failed' }
 
-  it { expect(subject.message).to eql 'Request failed' }
-  it { expect(subject.errors).to be_nil }
+  it { expect(request_error.message).to eql 'Request failed' }
+  it { expect(request_error.errors).to be_nil }
 
   context 'with error object' do
     let(:errors) { [{ 'code' => '3', 'message' => 'Requested service is not available' }] }
 
-    it { expect(subject.message).to eql '[{"code"=>"3", "message"=>"Requested service is not available"}]' }
-    it { expect(subject.errors).to eql errors }
+    it { expect(request_error.message).to eql '[{"code"=>"3", "message"=>"Requested service is not available"}]' }
+    it { expect(request_error.errors).to eql errors }
   end
 
   context 'without' do
     let(:errors) { nil }
 
-    it { expect(subject.message).to eql 'SecupayRuby::RequestError' }
-    it { expect(subject.errors).to be_nil }
+    it { expect(request_error.message).to eql 'SecupayRuby::RequestError' }
+    it { expect(request_error.errors).to be_nil }
   end
 end
